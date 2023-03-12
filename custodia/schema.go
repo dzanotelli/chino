76777b3d6cd285d9fc24b8f208efda8c507aa645
Daniele <dazano@gmail.com>
2023-03-12 21:18:56 +0100

    Implemented Schema create
    Refs #4

--- custodia/schema.go
+++ custodia/schema.go
@@ -1,6 +1,8 @@
 package custodia
 
 import (
+	"encoding/json"
+
 	"github.com/simplereach/timeutils"
 )
 
@@ -19,4 +21,35 @@ type Schema struct {
 	LastUpdate timeutils.Time `json:"last_update"`
 	IsActive bool `json:"is_active"`
 	Structure []SchemaField
+}
+
+type ScehamEnvelope struct {
+	Schema *Schema `json:"schema"`
+}
+
+type SchemasEnvelope struct {
+	Schemas []Schema `json:"schemas"`
+}
+
+// [C]reate a new schema
+func (ca *CustodiaAPIv1) CreateSchema(r Repository, descritpion string, 
+	fields []SchemaField, isActive bool) (*Schema, error) {
+	schema := Schema{RepositoryId: r.RepositoryId, Description: descritpion,
+		Structure: fields, IsActive: isActive}
+	data, err := json.Marshal(schema)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := ca.Call("POST", "/schemas", string(data))
+	if err != nil {
+		return nil, err
+	}
+
+	// JSON: unmarshal resp content
+	schemaEnvelope := ScehamEnvelope{}
+	if err := json.Unmarshal([]byte(resp), &schemaEnvelope); err != nil {
+		return nil, err
+	}
+
+	return schemaEnvelope.Schema, nil
 }
